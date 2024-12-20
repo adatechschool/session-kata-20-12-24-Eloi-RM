34e0e2e2f7faaf8895d8d5297bed2a437400d198
Eloi RM <eloimailmaynad@gmail.com>
2024-12-20 10:48:25 +0100

    fix : function return and typo

--- javascript/combinationVerification.mjs
+++ javascript/combinationVerification.mjs
@@ -1,13 +1,14 @@
 import { secretCombination } from "./data.mjs";
 
-function verifiatePlayerCombination() {
-    return isPlayerCombinationTrue(myArray)
+function verifiatePlayerCombination(playerCombination) {
+    console.log("j")
+    return isPlayerCombinationTrue(playerCombination)
 }
 
 function isPlayerCombinationTrue(myArray) {
     console.log(secretCombination)
     console.log(myArray)
-    return myArray === secretCombination
+    return myArray == secretCombination
 }
 
 export {
--- javascript/inputVerification.mjs
+++ javascript/inputVerification.mjs
@@ -14,6 +14,7 @@ function verfiatePlayerInput(playerCombination) {
         return false
     }
     console.log(`is valid colors`)
+    return true
 }
 
 
